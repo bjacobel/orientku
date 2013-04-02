e6cc1d7b402fa709a44e7dc2ee2bfbc70f781889
Brian Jacobel <bjacobel@gmail.com>
2013-04-02 01:44:11 -0400

    blocking script from tweeting the same line twice

--- orientku.py
+++ orientku.py
@@ -5,8 +5,15 @@ from haikufinder import HaikuFinder
 
 
 text = "the nation's oldest continuously published college newspaper"
+usedlines = open("used.txt", "r").read()
 
 haikus = HaikuFinder(text).find_haikus()
 for haiku in haikus:
-	cmd = "t update \"%s \n     %s \n%s\"" %(haiku[0],haiku[1],haiku[2])
-	os.system(cmd)
+	if(haiku[0] not in usedlines and haiku[1] not in usedlines and haiku[2] not in usedlines):
+	        #tweet tweet
+		new = "t update \"%s \n     %s \n%s\"" %(haiku[0],haiku[1],haiku[2])
+                #os.system(new)
+
+	        #blacklist all thre lines you just tweeted
+		used = "echo \"%s\">>used.txt && echo \"%s\">>used.txt && echo \"%s\">>used.txt" %(haiku[0],haiku[1],haiku[2])
+		os.system(used)
