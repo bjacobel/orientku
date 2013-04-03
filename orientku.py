7a288f744c7863770c530f740da5d174015631c6
Brian Jacobel <bjacobel@gmail.com>
2013-04-03 15:08:58 -0400

    should be ready to go as soon as apiv2.0 is online\!

--- orientku.py
+++ orientku.py
@@ -2,26 +2,42 @@
 import sys
 import os
 import urllib
+import json
+import random
 from haikufinder import HaikuFinder
-from xml.dom import minidom
 
-#pull random articles from "volume CXLII" of the Orient
-content = urllib.urlopen("http://bowdoinorient.com/api/xml_issuelist/CXLII")
-issues = minidom.parse(content)
-dates = issues.getElementById('0')
-print dates
+#pull a random article from "volume CXLII" of the Orient
+content = urllib.urlopen("http://bowdoinorient.dev/api/json_issuelist/CXLII").read()
+dates = json.loads(content)
+maxissue = 0
+for date in dates:
+	if (int(date['issue_number']) > maxissue):
+		maxissue = int(date['issue_number'])
+randissue = random.randrange(0, maxissue, 1)
+randdate = dates[randissue]['issue_date']
+nexturl = "http://bowdoinorient.dev/api/json_fulltext/"+randdate+"/"+str(random.randrange(1,5,1))
+content = urllib.urlopen(nexturl).read()
+sectiontext = json.loads(content)
+randarticle = random.randrange(0, len(sectiontext), 1)
+text = sectiontext[randarticle]["body"].encode('ascii', 'ignore')
 
-text = "the nation's oldest continuously published college newspaper"
+#text file keeps track of lines we've used
 usedlines = open("used.txt", "r").read()
 
 #find the haikus
 haikus = HaikuFinder(text).find_haikus()
-for haiku in haikus:
+
+#pick a random one if more than one was found
+tweeted = 0
+
+while not tweeted:
+	haiku = haikus[random.randrange(0, len(haikus), 1)]
 	if(haiku[0] not in usedlines and haiku[1] not in usedlines and haiku[2] not in usedlines):
 	        #tweet tweet motherfuckers
 		new = "t update \"%s \n     %s \n%s\"" %(haiku[0],haiku[1],haiku[2])
-                #os.system(new)
+		os.system(new)
 
-	        #blacklist all thre lines you just tweeted
+	       #blacklist all the lines you just tweeted
 		used = "echo \"%s\">>used.txt && echo \"%s\">>used.txt && echo \"%s\">>used.txt" %(haiku[0],haiku[1],haiku[2])
 		os.system(used)
+		tweeted = 1
