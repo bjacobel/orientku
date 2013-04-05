44efe9515a9cf4fcf0b7b95cc44db808cae4d581
Brian Jacobel <bjacobel@gmail.com>
2013-04-04 21:03:48 -0400

    enclosed the whole thing in a try/catch, whatever

--- orientku.py
+++ orientku.py
@@ -14,11 +14,8 @@ while not tweeted:
 		content = urllib.urlopen("http://bowdoinorient.com/api/json_volumelist").read()
 		volumes = json.loads(content)
 		volume = volumes[random.randrange(0,len(volumes))]["roman"]
-	except ValueError:
-		break
 
-        #pull a random article from that volume of the Orient
-	try:
+                #pull a random article from that volume of the Orient
 		content = urllib.urlopen("http://bowdoinorient.com/api/json_issuelist/"+str(volume)).read()
 		dates = json.loads(content)
 		maxissue = 0
@@ -27,45 +24,41 @@ while not tweeted:
 				maxissue = int(date['issue_number'])
 		randissue = random.randrange(0, maxissue)
 		randdate = dates[randissue]['issue_date']
-	except IndexError:
-		break
+		nexturl = "http://bowdoinorient.com/api/json_fulltext/"+randdate+"/"+str(random.randrange(1,5,1))
+		content = urllib.urlopen(nexturl).read()
+		sectiontext = json.loads(content)
+		randarticle = random.randrange(0, len(sectiontext))
+		text = sectiontext[randarticle]["body"].encode('ascii', 'ignore')
 
+		url = "http://bowdoinorient.com/article/" + sectiontext[randarticle]["id"]
+		print "Searching %s for haikus..." %url
 
-	nexturl = "http://bowdoinorient.com/api/json_fulltext/"+randdate+"/"+str(random.randrange(1,5,1))
-	content = urllib.urlopen(nexturl).read()
-	sectiontext = json.loads(content)
-	randarticle = random.randrange(0, len(sectiontext))
-	text = sectiontext[randarticle]["body"].encode('ascii', 'ignore')
+                #text file keeps track of lines we've used
+		usedlines = open("used.txt", "r").read()
 
-	url = "http://bowdoinorient.com/article/" + sectiontext[randarticle]["id"]
-	print "Searching %s for haikus..." %url
+                #find the haikus
+		haikus = HaikuFinder(text).find_haikus()
 
-        #text file keeps track of lines we've used
-	usedlines = open("used.txt", "r").read()
+                #pick a random one if more than one was found
+		trycount = 0
 
-        #find the haikus
-	haikus = HaikuFinder(text).find_haikus()
+		if haikus:
+			print "%s haikus found in this article. Trying to tweet one of them..." %len(haikus)
+			while (not tweeted) and (trycount <= len(haikus)):
+				trycount+=1
+				haiku = haikus[random.randint(0, len(haikus))]
+				if(haiku[0] not in usedlines and haiku[1] not in usedlines and haiku[2] not in usedlines):
+	                                #tweet tweet motherfuckers
+					new = "t update \"%s \n     %s \n%s\n%s\"" %(haiku[0],haiku[1],haiku[2],url)
+					os.system(new)
 
-        #pick a random one if more than one was found
-	trycount = 0
-
-	if haikus:
-		print "%s haikus found in this article. Trying to tweet one of them..." %len(haikus)
-		while (not tweeted) and (trycount <= len(haikus)):
-			trycount+=1
-			haiku = haikus[random.randint(0, len(haikus))]
-			if(haiku[0] not in usedlines and haiku[1] not in usedlines and haiku[2] not in usedlines):
-	                        #tweet tweet motherfuckers
-				new = "t update \"%s \n     %s \n%s\n%s\"" %(haiku[0],haiku[1],haiku[2],url)
-				os.system(new)
-
- 	                        #blacklist all the lines you just tweeted
-				used = "echo \"%s\">>used.txt && echo \"%s\">>used.txt && echo \"%s\">>used.txt" %(haiku[0],haiku[1],haiku[2])
-				os.system(used)
-				tweeted = 1
-				print "Tweeted!"
-		if not tweeted:
-			print "The only found tweet was already tweeted :("
-	else:
-		print "No haikus found :("
+ 	                                #blacklist all the lines you just tweeted
+					used = "echo \"%s\">>used.txt && echo \"%s\">>used.txt && echo \"%s\">>used.txt" %(haiku[0],haiku[1],haiku[2])
+					os.system(used)
+					tweeted = 1
+					print "Tweeted!"
+			if not tweeted:
+				print "The only found tweet was already tweeted :("
+		else:
+			print "No haikus found :("
 
