ef77e65cb97edf588c770dd9535be14513f4dd56
Brian Jacobel <bjacobel@gmail.com>
2013-09-18 15:10:37 -0400

    Rewriting a lot of this shit python I wrote before I actually knew how to use Python

--- orientku.py
+++ orientku.py
@@ -9,75 +9,77 @@ from haikufinder import HaikuFinder
 ## LET IT BE KNOWN that this was the second Python program I ever wrote.
 ## Don't judge me, bro.
 
+USED_LOG = "./used.txt"
 
-tweeted = 0
+tweeted = False
+
+content = urllib.urlopen("http://bowdoinorient.com/api/json_volumelist").read()
+volumes = json.loads(content)[1:]  # volume CXXXII doesn't actually have issues in it in the API
 
 while not tweeted:
     # pick a volume
+    volume = volumes[random.randrange(0,len(volumes))]["roman"]
+
+    #pull a random article from that volume
+    content = urllib.urlopen("http://bowdoinorient.com/api/json_issuelist/"+str(volume)).read()
+    dates = json.loads(content)
+
+    maxissue = int(dates.pop()["issue_number"])
+
+    randissue = random.randrange(0, maxissue)
+    
     try:
-        content = urllib.urlopen("http://bowdoinorient.com/api/json_volumelist").read()
-        volumes = json.loads(content)
-        volume = volumes[random.randrange(0,len(volumes))]["roman"]
-
-        #pull a random article from that volume of the Orient
-        content = urllib.urlopen("http://bowdoinorient.com/api/json_issuelist/"+str(volume)).read()
-        dates = json.loads(content)
-        maxissue = 0
-        for date in dates:
-            if (int(date['issue_number']) > maxissue):
-                maxissue = int(date['issue_number'])
-        randissue = random.randrange(0, maxissue)
         randdate = dates[randissue]['issue_date']
-        nexturl = "http://bowdoinorient.com/api/json_fulltext/"+randdate+"/"+str(random.randrange(1,5,1))
-        content = urllib.urlopen(nexturl).read()
-        sectiontext = json.loads(content)
-        randarticle = random.randrange(0, len(sectiontext))
-        text = sectiontext[randarticle]["body"].encode('ascii', 'ignore')
-        # text = "the nation's \"oldest\" continuously published college newspaper"
-        # text = open("sometextfile.txt").read()
+    except:
+        print "I've made a terrible mistake. (Vol. {} no. {})\n".format(131 + int(volumes[random.randrange(0,len(volumes))]["id"]), randissue)
+        continue
+   
+    nexturl = "http://bowdoinorient.com/api/json_fulltext/"+randdate+"/"+str(random.randrange(1,5,1))
+    print "Calling the API..."
 
-        url = "http://bowdoinorient.com/article/" + sectiontext[randarticle]["id"]
-        print "Searching %s for haikus..." %url
+    content = urllib.urlopen(nexturl).read()
+    sectiontext = json.loads(content)
+    randarticle = random.randrange(0, len(sectiontext))
+    text = sectiontext[randarticle]["body"].encode('ascii', 'ignore')
 
-        #text file keeps track of lines we've used
-        usedlines = open("/home/bjacobel/projects/orientku/used.txt", "r").read()
+    url = "http://bowdoinorient.com/article/" + sectiontext[randarticle]["id"]
+    print "Searching %s for haikus..." %url
 
-        #find the haikus
-        haikus = HaikuFinder(text).find_haikus()
+    # text file keeps track of lines we've used
+    usedlines = open(USED_LOG, "r").read()
 
-        #pick a random one if more than one was found
-        trycount = 0
+    # find the haikus
+    haikus = HaikuFinder(text).find_haikus()
 
-        if haikus:
-            print "%s haikus found in this article. Trying to tweet one of them..." %len(haikus)
-            while (not tweeted) and (trycount <= len(haikus)):
-                trycount += 1
-                haiku = haikus[random.randrange(0, len(haikus), 1)]
-                
-                # parse double quotes
-                haiku[0] = haiku[0].replace('"', '\\"')
-                haiku[1] = haiku[1].replace('"', '\\"')
-                haiku[2] = haiku[2].replace('"', '\\"')
-                
-                if(haiku[0] not in usedlines and haiku[1] not in usedlines and haiku[2] not in usedlines):
-                    # tweet tweet motherfuckers
-                    new = "twitter set \"%s \n     %s \n%s\n%s\"" %(haiku[0],haiku[1],haiku[2],url)
-                    os.system(new)
+    # pick a random one if more than one was found
+    trycount = 0
 
-                    # blacklist all the lines you just tweeted
-                    # Like I said earlier.... don't judge me for this.
-                    used = "echo \"%s\" >> ~/projects/orientku/used.txt && echo \"%s\" >> ~/projects/orientku/used.txt && echo \"%s\" >> ~/projects/orientku/used.txt" %(haiku[0],haiku[1],haiku[2])
-                    os.system(used)
-                    tweeted = 1
-                    print "Tweeted!"
-            if not tweeted:
-                print "The only found tweet was already tweeted :("
-        else:
-            print "No haikus found :("
+    if haikus:
+        print "%s haiku(s) found in this article. Trying to tweet one of them..." %len(haikus)
+        while (not tweeted) and (trycount <= len(haikus)):
+            trycount += 1
+            haiku = haikus[random.randrange(0, len(haikus), 1)]
+            
+            # parse double quotes
+            haiku[0] = haiku[0].replace('"', '\\"')
+            haiku[1] = haiku[1].replace('"', '\\"')
+            haiku[2] = haiku[2].replace('"', '\\"')
+            
+            if(haiku[0] not in usedlines and haiku[1] not in usedlines and haiku[2] not in usedlines):
+                # tweet tweet motherfuckers
+                new = 'twitter set "{} \n     {} \n{}\n{}"'.format(haiku[0], haiku[1], haiku[2], url)
+                os.system(new)
 
-    except IndexError:
-        #print "gracefully handled an IndexError"
-        raise
-    except ValueError:
-        #print "gracefully handled a ValueError"
-        raise
+                # blacklist all the lines you just tweeted
+                # Like I said earlier.... don't judge me for this.
+                used = 'echo "{line1}" >> {logfile} && echo "{line2}" >> {logfile} && echo "{line3}" >> {logfile}'.format(line1 = haiku[0], line2 = haiku[1], line3 = haiku[2], logfile = USED_LOG)
+                os.system(used)
+                tweeted = True
+                print "Tweeted!"
+
+        if not tweeted:
+            print "That tweet was already tweeted :("
+    else:
+        print "No haikus found :("
+
+    print ""
